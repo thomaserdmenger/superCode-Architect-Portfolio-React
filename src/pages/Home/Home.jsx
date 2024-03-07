d327b1eb35abeef71633872e5572e786523223c9
thomaserdmenger <thomas.erdmenger@googlemail.com>
2024-03-07 15:55:11 +0100

    Add Hero Section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,19 @@
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 const Home = () => {
   return (
     <div className='wrapper'>
-      <main>
-        <h1>Home Page</h1>
+      <main className='home'>
+        <section className='home__hero'>
+          <div className='home__hero__content-container'>
+            <p>Project</p>
+            <p>Lorum</p>
+          </div>
+          <div className='home__hero__image-container'>
+            <Link to='/projects'>View Project</Link>
+          </div>
+        </section>
       </main>
     </div>
   )
